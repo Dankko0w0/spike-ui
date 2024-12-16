8c749db5ecda98f00cd2e5814578b8d23ac78010
yejh <yejh@finchina.com>
2024-12-16 15:18:32 +0800

    实现Footer组件

--- src/Layout/Footer.jsx
+++ src/Layout/Footer.jsx
@@ -1,7 +1,14 @@
 function Footer() {
   return (
-    <p>Footer</p>
+    <footer className="footer footer-center bg-base-300 text-base-content p-4">
+      <aside>
+        <p>
+        🩷 Copyright © {new Date().getFullYear()} - All right reserved by Financial China Information & Technology Co., Ltd.
+        </p>
+      </aside>
+    </footer>
   );
 }
 
-export default Footer; 
+export default Footer;
+
